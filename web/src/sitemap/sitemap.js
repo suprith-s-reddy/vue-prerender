7ac7507cf6d945c5433fca0f45252939fe749935
Suprith Reddy <sr@ofinow.com>
2020-04-11 23:06:44 +0200

    added placeholders for dynamic slugs from api

--- web/src/sitemap/sitemap.js
+++ web/src/sitemap/sitemap.js
@@ -1,17 +1,26 @@
+const axios = require('axios');
 const fs = require('fs')
 const routes = require('../common/routes')
 
 async function generateSitemap () {
   let links = []
   links = getVueRoutes()
+  // const blogsSlugsRes = await getBlogsSlugs();
+  // links = links.concat(blogSlugs.data.data);
   writeSlugsToSitemap(links)
 };
 
 function getVueRoutes () {
   const vueRoutes = Object.values(routes.paths)
-
   return vueRoutes
 }
+
+// function getBlogsSlugs() {
+//   return axios({
+//       method: 'get',
+//       url: `https://api.example.com/blogs/slugs/list` // your api url goes here
+//   });
+// };
 
 function writeSlugsToSitemap (routes) {
   try {
@@ -23,5 +32,4 @@ function writeSlugsToSitemap (routes) {
   }
 }
 
-// Generating Sitemap.
 generateSitemap()
